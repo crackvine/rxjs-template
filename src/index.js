3470b275fa0017fdd9223bf3cbfdfd4ea052c45c
farivar.parand <farivar.parand@guidesmiths.com>
2020-12-29 09:19:10 +0100

    More illustrative sample

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import { interval } from 'rxjs';
-import { map, filter } from 'rxjs/operators';
+import { map, filter, takeWhile } from 'rxjs/operators';
 
-const loggerSubscription = (x) => {
-  console.log(x);
+const loggerSubscription = {
+  next: (x) => {
+    console.log(x);
+  },
+  complete: () => { console.log('done'); },
+  error: (error) => { console.error(error); },
 };
 
 const ticker$ = interval(2000).pipe(
+  takeWhile((x) => x < 5),
   map((x) => x * 2 + x),
   filter((x) => x % 2 === 0),
 ).subscribe(loggerSubscription);
